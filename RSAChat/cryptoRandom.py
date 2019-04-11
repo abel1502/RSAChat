8dd652cd64a0d5401a330c14f7f57b1ba0947bb4
abel1502 <terragame.abel@gmail.com>
2019-04-11 14:40:28 +0300

    Hotfix

--- RSAChat/cryptoRandom.py
+++ RSAChat/cryptoRandom.py
@@ -7,6 +7,7 @@ class RandomSeeder:
     def Initialize():
         if not RandomSeeder.seeded:
             random.seed(os.urandom(256))
+            RandomSeeder.seeded = True
 
 
 RandomSeeder.Initialize()
