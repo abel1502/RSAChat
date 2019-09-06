ca1c7aa42d2de6fe43d4912fd4a6061456625ea1
abel1502 <terragame.abel@gmail.com>
2019-09-06 21:36:01 +0300

    Protocol redesign
    Todo: edit (or remove) EPACKET, get rid of EPACKET_TYPE references.
    Smth like that

--- RSAChat/protocol.py
+++ RSAChat/protocol.py
@@ -9,13 +9,13 @@ from . import RSA
 import hashlib
 
 
-class EPACKET_TYPE: # Inheritance from Enum?
-    REGULAR = 0
-    HSH_CL_ASK = 1
-    HSH_SRV_ANS = 2
-    HSH_CL_SIMPLE = 3
-    HSH_VER_ASK = 4
-    HSH_VER_ANS = 5
+#class EPACKET_TYPE: # Inheritance from Enum?
+    #REGULAR = 0
+    #HSH_CL_ASK = 1
+    #HSH_SRV_ANS = 2
+    #HSH_CL_SIMPLE = 3
+    #HSH_VER_ASK = 4
+    #HSH_VER_ANS = 5
 
 
 class BasePacket:
@@ -52,6 +52,14 @@ class BasePacket:
                     value = self.fields[elem[0]][-elem[2]:]
                     value = b'\x00' * (elem[2] - len(value)) + value
                     result += value
+            elif elem[1] == RSA.PublicKey:
+                raw = self.fields[elem[0]].dump().encode()
+                if elem[2] < 0:
+                    result += len(raw).to_bytes(-elem[2], "big") + raw
+                else:
+                    value = raw[-elem[2]:]
+                    value = b'\x00' * (elem[2] - len(value)) + value
+                    result += value
             else:
                 utils.raiseException("protocol.BasePacket.encode", "Not implemented")
         return result
@@ -82,12 +90,117 @@ class BasePacket:
                         return None
                     result.fields[elem[0]] = buf[ptr:ptr + elem[2]]
                     ptr += elem[2]
+            elif elem[1] == RSA.PublicKey:
+                if elem[2] < 0:
+                    if ptr - elem[2] > len(buf):
+                        return None
+                    length = int.from_bytes(buf[ptr:ptr - elem[2]], "big")
+                    ptr += -elem[2]
+                    if ptr + length > len(buf):
+                        return None                
+                    result.fields[elem[0]] = RSA.PublicKey.load(buf[ptr:ptr + length].decode())
+                    ptr += length
+                else:
+                    if ptr + elem[2] > len(buf):
+                        return None
+                    result.fields[elem[0]] = RSA.PublicKey.load(buf[ptr:ptr + elem[2]].decode())
+                    ptr += elem[2]
             else:
-                utils.raiseException("protocol.BasePacket.encode", "Not implemented")
+                utils.raiseException("protocol.BasePacket._parse", "Not implemented")
         return ptr, result
     
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, ', '.join(map(lambda x: "{}={}".format(x, repr(self.fields[x])), self.fields)))
+
+
+class HSH_CL_ASK_PACKET(BasePacket):
+    structure = [("CL_PKEY", RSA.PublicKey, -2)]
+    defaultFields = {"CL_PKEY": None}
+    
+    @staticmethod
+    def build(clientKey):
+        utils.checkParamTypes("protocol.HSH_CL_ASK_PACKET.build", [clientKey], [{bytes, str, RSA.PublicKey}])
+        if isinstance(clientKey, RSA.PublicKey):
+            clientKey = clientKey.dump().encode()
+        elif isinstance(clientKey, str):
+            clientKey = clientKey.encode()
+        return HSH_CL_ASK_PACKET(CL_PKEY=clientKey)
+    
+    @classmethod
+    def parse(cls, buf):
+        return cls._parse(buf)
+
+
+class HSH_SRV_ANS_PACKET(BasePacket):
+    structure = [("S_PKEY", bytes, -2)]
+    defaultFields = {"S_PKEY": None}
+    
+    @staticmethod
+    def build(serverKey, clientKey):
+        utils.checkParamTypes("protocol.HSH_SRV_ANS_PACKET.build", [serverKey, clientKey], [{bytes, str, RSA.PublicKey}, {bytes, str, RSA.PublicKey}])
+        if isinstance(serverKey, RSA.PublicKey):
+            serverKey = serverKey.dump().encode()
+        elif isinstance(serverKey, str):
+            serverKey = serverKey.encode()
+        clientKey = RSA.load(clientKey)
+        serverKey = clientKey.encrypt(serverKey)
+        return HSH_SRV_ANS_PACKET(S_PKEY=serverKey)
+    
+    @classmethod
+    def parse(cls, buf):
+        return cls._parse(buf)
+
+
+class HSH_CL_SIMPLE_PACKET(BasePacket):
+    structure = [("CL_PKEY", bytes, -2)]
+    defaultFields = {"CL_PKEY": None}
+    
+    @staticmethod
+    def build(clientKey, serverKey):
+        utils.checkParamTypes("protocol.HSH_CL_SIMPLE_PACKET.build", [clientKey], [{bytes, str, RSA.PublicKey}])
+        if isinstance(clientKey, RSA.PublicKey):
+            clientKey = clientKey.dump().encode()
+        elif isinstance(clientKey, str):
+            clientKey = clientKey.encode()
+        serverKey = RSA.load(serverKey)
+        clientKey = serverKey.encrypt(clientKey)
+        return HSH_CL_SIMPLE_PACKET(CL_PKEY=clientKey)
+    
+    @classmethod
+    def parse(cls, buf):
+        return cls._parse(buf)
+
+
+class HSH_VER_ASK_PACKET(BasePacket):
+    structure = [("CHALLENGE", bytes, -2)]
+    defaultFields = {"CHALLENGE": None}
+    
+    @staticmethod
+    def build(challenge, clientKey):
+        utils.checkParamTypes("protocol.HSH_VER_ASK_PACKET.build", [challenge, clientKey], [{bytes}, {bytes, str, RSA.PublicKey}])
+        clientKey = RSA.load(clientKey)
+        challenge = clientKey.encrypt(challenge)
+        return HSH_VER_ASK_PACKET(CHALLENGE=challenge)
+    
+    @classmethod
+    def parse(cls, buf):
+        return cls._parse(buf)
+
+
+class HSH_VER_ANS_PACKET(BasePacket):
+    structure = [("SOLUTION", bytes, -2)]
+    defaultFields = {"SOLUTION": None}
+    
+    @staticmethod
+    def build(solution, serverKey):
+        utils.checkParamTypes("protocol.HSH_VER_ANS_PACKET.build", [solution, serverKey], [{bytes}, {bytes, str, RSA.PublicKey}])
+        serverKey = RSA.load(serverKey)
+        solution = serverKey.encrypt(solution)
+        return HSH_VER_ANS_PACKET(SOLUTION=solution)
+    
+    @classmethod
+    def parse(cls, buf):
+        return cls._parse(buf)
 
 
 class EPACKET(BasePacket):
@@ -134,7 +247,6 @@ class SPACKET(BasePacket):
         if ptr < len(buf):
             utils.raiseException("protocol.SPACKET.parse", "Buf contains extra information")
         return packet
-        
 
 
 class PPACKET(BasePacket):
@@ -169,3 +281,44 @@ class PPACKET(BasePacket):
             utils.raiseException("protocol.PPACKET.parse", "Buf contains extra information")
         return packet    
 
+
+class ProtocolNode:
+    def __init__(self, pType, sendNode=True, children=[], preProcess=(lambda *args: None), postProcess=(lambda *args: None)):
+        self.pType = pType
+        self.sendNode = sendNode
+        self.children = children
+        self.nextId = 0
+        self.preProcess = preProcess
+        self.postProcess = postProcess
+    
+    def apply(self, networkHandler):
+        if self.sendNode:
+            packet = self.preProcess(networkHandler)
+            networkHandler.send(packet)
+            self.postProcess(networkHandler)
+        else:
+            self.preProcess(networkHandler)
+            packet = networkHandler.parseRecv(pType)
+            self.postProcess(networkHandler, packet)
+    
+    def next(self):
+        if self.isTerm():
+            return None
+        return self.children[self.nextId]
+    
+    def isTerm(self):
+        return len(self.children) == 0
+
+
+class ProtocolHandler:
+    def __init__(self, protocolTree):
+        self.curNode = protocolTree
+        self.networkHandler = None
+    
+    def wrap(self, networkHandler):
+        self.networkHandler = networkHandler
+    
+    def start(self):
+        while self.curNode is not None:
+            self.curNode.apply(networkHandler)
+            self.curNode = self.curNode.next()
